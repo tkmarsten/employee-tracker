4d8b3c373646fccf8c8eb5af1448cac1d70406ea
tkmarsten <tkmarsten@gmail.com>
2022-11-03 00:02:47 -0700

    Update to appropriate dummy data

--- db/insert.sql
+++ db/insert.sql
@@ -1,13 +1,17 @@
 INSERT INTO department (name)
-VALUES  ('goofs'),
-        ('gaffs'),
-        ('laughs');
+VALUES  ('Front End'),
+        ('Back End'),
+        ('Fullstack');
 
 INSERT INTO roles (title, salary, department_id)
-VALUES  ('memes', 30000.00, (SELECT id FROM department WHERE name = 'laughs')),
-        ('pranks', 20000.00, (SELECT id FROM department WHERE name = 'goofs')),
-        ('funnies', 40000.00, (SELECT id FROM department WHERE name = 'gaffs'));
+VALUES  ('Database', 80000.00, (SELECT id FROM department WHERE name = 'Back End')),
+        ('UI / UX', 70000.00, (SELECT id FROM department WHERE name = 'Front End')),
+        ('Server', 120000.00, (SELECT id FROM department WHERE name = 'Back End')),
+        ('E-Commerce', 100000.00, (SELECT id FROM department WHERE name = 'Fullstack'));
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES  ('manager', 'face', (SELECT id FROM roles WHERE title = 'memes'), null),
-        ('boo', 'bear', (SELECT id FROM roles WHERE title = 'memes'), 1);
+VALUES  ('People', 'Manager', (SELECT id FROM roles WHERE title = 'Server'), null),
+        ('Foo', 'Bar', (SELECT id FROM roles WHERE title = 'Server'), 1),
+        ('John', 'Dough', (SELECT id FROM roles WHERE title = 'Server'), 1),
+        ('Money', 'Bags', (SELECT id FROM roles WHERE title = 'E-Commerce'), null),
+        ('Jane', 'Foe', (SELECT id FROM roles WHERE title = 'Database'), null);
--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE department (
 CREATE TABLE roles (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
+  salary DECIMAL NOT NULL,
   department_id INT,
   FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
@@ -21,6 +21,6 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INT,
-  manager_id INT,
+  manager_id INT DEFAULT NULL,
   FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
 );
